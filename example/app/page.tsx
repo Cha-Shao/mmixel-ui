3d6217f4edcc02ebdc1989203dbeecc457ba9178
ChaShao <yrchashao@outlook.com>
2024-06-02 00:40:34 +0800

    upd

--- example/app/page.tsx
+++ example/app/page.tsx
@@ -1,7 +1,7 @@
 const Page = () => {
   return (
     <main>
-      hi
+
     </main>
   )
 }
--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,5 +1,12 @@
 import classNames = require("classnames")
 
+enum ButtonSize {
+  Tiny = 'ty',
+  Small = 'sm',
+  Medium = 'md',
+  Large = 'lg',
+}
+
 enum ButtonVariant {
   Solid = 'solid',
   Secondary = 'secondary',
@@ -9,12 +16,16 @@ enum ButtonVariant {
 
 const Button = ({
   variant,
+  size,
   ...attrs
 }) => {
   return (
     <button
       className={classNames(
-
+        size === ButtonSize.Tiny && 'px-2 py-1 text-xs',
+        size === ButtonSize.Small && 'px-3 py-2 text-sm',
+        size === ButtonSize.Medium && 'px-4 py-2 text-base',
+        size === ButtonSize.Large && 'px-6 py-3 text-lg',
       )}
       {...attrs}
     >
