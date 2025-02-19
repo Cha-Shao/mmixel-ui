097e202e2d0052a0559e87e0dc58e81c5e183040
ChaShao <yrchashao@outlook.com>
2025-02-20 03:19:25 +0800

    form

---
+++ components/Form/FormItem.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import classNames from "classnames"
+import { HTMLAttributes } from "react"
+
+export interface FormItemProps extends HTMLAttributes<HTMLDivElement> {
+  label: string
+  htmlFor?: string
+  required?: boolean
+}
+
+const FormItem = ({
+  label,
+  htmlFor,
+  required,
+  ...props
+}: FormItemProps) => {
+  return (
+    <div {...props}>
+      <label htmlFor={htmlFor} className={classNames(
+        "block mb-2 relative",
+        required && "before:content-['*'] before:absolute before:-left-3 before:text-dangerous",
+      )}>
+        {label}
+      </label>
+      {props.children}
+    </div>
+  )
+}
+
+export default FormItem
---
+++ components/Form/FormList.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import classNames from "classnames"
+import { FormHTMLAttributes } from "react"
+
+const FormList = (
+  props: FormHTMLAttributes<HTMLFormElement>
+) => {
+  return (
+    <form
+      {...props}
+      className={classNames(
+        "grid gap-4",
+        props.className
+      )}
+      onSubmit={e => {
+        props.onSubmit
+          ? props.onSubmit(e)
+          : e.preventDefault()
+      }}
+    >
+      {props.children}
+    </form>
+  )
+}
+
+export default FormList
